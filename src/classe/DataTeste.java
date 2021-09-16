8df8ab40fca774cab5f2ab945ca368c3757f49bb
Luiz Souza <luixsouza12@outlook.com>
2021-09-16 11:48:19 -0300

    update classe
    update classe / create desafio_modulo

--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -7,12 +7,13 @@ public class DataTeste {
     Data d1 = new Data();
     d1.ano = 2021;
 
-    var d2 = new Data(21, 12, 2020);
-
+    var d2 = new Data(31, 12, 2020);
 
     String dataFormatada1 = d1.obterDataFormatada();
     System.out.println(dataFormatada1);
     System.out.println(d2.obterDataFormatada());
 
+    d1.imprimirDataFormatada();
+    d2.imprimirDataFormatada();
   }
 }
