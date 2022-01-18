01317c1ca800fe13c8346fc986da630bc3b4d79c
Luiz Souza <luixsouza12@outlook.com>
2022-01-17 23:23:04 -0300

    criando desafio de composição

---
+++ src/oo/composicao/desafio/Item.java
@@ -0,0 +1,15 @@
+package oo.composicao.desafio;
+
+import java.util.ArrayList;
+
+public class Item{
+
+    final Produto produto;
+    final int quantidade;
+
+    Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+}
